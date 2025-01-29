60e2197f0cd7391fd768306adc849f4420a31f9c
fady emil <fadymobiledev@gmail.com>
2025-01-29 17:26:24 +0200

    done ui category

--- lib/features/home/presentation/view/widget/category_body.dart
+++ lib/features/home/presentation/view/widget/category_body.dart
@@ -1,25 +1,40 @@
+import 'package:e_commerce/features/home/presentation/manger/categories_riverpod.dart';
 import 'package:e_commerce/features/home/presentation/view/widget/category_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CategoryBody extends StatelessWidget {
+class CategoryBody extends ConsumerWidget {
   const CategoryBody({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context) {
-    return GridView.builder(
-      itemCount: 5,
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        crossAxisSpacing: 8,
-        childAspectRatio: 0.8,
-        mainAxisSpacing: 8,
-      ),
-      itemBuilder: (context, index) {
-        return CategoryItem();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final categoriesAsync = ref.watch(CategoriesProvider);
+    return categoriesAsync.when(
+      data: (categories) {
+        return GridView.builder(
+          itemCount: categories.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 8,
+            childAspectRatio: 0.8,
+            mainAxisSpacing: 8,
+          ),
+          itemBuilder: (context, index) {
+            return CategoryItem(
+              cate: categories[index],
+            );
+          },
+        );
+      },
+      loading: () => const Center(child: CircularProgressIndicator()),
+      error: (err, stack) {
+        print('Error: $err');
+        return Center(
+          child: Text('Error: ${err.toString()}'),
+        );
       },
     );
   }
 }
-
--- lib/features/home/presentation/view/widget/category_item.dart
+++ lib/features/home/presentation/view/widget/category_item.dart
@@ -1,13 +1,16 @@
-
-import 'package:e_commerce/core/constants/images.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:e_commerce/core/theme/app_text_styles.dart';
+import 'package:e_commerce/features/home/domain/categories_entity.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CategoryItem extends StatelessWidget {
   const CategoryItem({
     super.key,
+    required this.cate,
   });
+
+  final CategoriesEntity cate;
 
   @override
   Widget build(BuildContext context) {
@@ -16,21 +19,39 @@ class CategoryItem extends StatelessWidget {
         borderRadius: BorderRadius.circular(12.r),
       ),
       elevation: 3,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Expanded(
-            child: Image.asset(
-              Assets.imageCheck,
-              fit: BoxFit.cover,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(12.r),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: CachedNetworkImage(
+                imageUrl: cate.image,
+                imageBuilder: (context, imageProvider) => Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12.r),
+                    image: DecorationImage(
+                      image: imageProvider,
+                      fit: BoxFit.cover,
+                      colorFilter: ColorFilter.mode(
+                        Colors.brown.withValues(alpha: 0.5),
+                        BlendMode.softLight,
+                      ),
+                    ),
+                  ),
+                ),
+                placeholder: (context, url) =>
+                    Center(child: CircularProgressIndicator()),
+                errorWidget: (context, url, error) => Icon(Icons.error),
+              ),
             ),
-          ),
-          const SizedBox(height: 8),
-          Text(
-            "name of category",
-            style: AppTextStyles.bodyBasaSemiBold16,
-          ),
-        ],
+            const SizedBox(height: 8),
+            Text(
+              cate.name,
+              style: AppTextStyles.bodyBasaSemiBold16,
+            ),
+          ],
+        ),
       ),
     );
   }
