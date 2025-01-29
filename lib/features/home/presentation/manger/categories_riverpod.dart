a869b076cf5e2794e5572f5b7238b38ef6b2e7fc
fady emil <fadymobiledev@gmail.com>
2025-01-29 17:26:43 +0200

    manger category done

---
+++ lib/features/home/presentation/manger/categories_riverpod.dart
@@ -0,0 +1,23 @@
+import 'package:e_commerce/core/DI/dependency_injection.dart';
+import 'package:e_commerce/core/services/api/api_comsumer.dart';
+import 'package:e_commerce/features/home/data/categories_repo_impl.dart';
+import 'package:e_commerce/features/home/domain/categories_entity.dart';
+import 'package:e_commerce/features/home/domain/categories_repo.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final CategoriesProvider = FutureProvider<List<CategoriesEntity>>((ref) async {
+  final categoryRepo = ref.watch(categoryRepoProvider);
+
+  final result = await categoryRepo.getCategories();
+
+  return result.fold(
+    (failure) {
+      throw Exception(failure);
+    },
+    (characters) => characters,
+  );
+});
+
+final categoryRepoProvider = Provider<CategoriesRepo>((ref) {
+  return CategoriesRepoImpl(apiConsumer: getIt<ApiConsumer>());
+});
