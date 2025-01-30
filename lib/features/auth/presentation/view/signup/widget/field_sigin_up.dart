840119361e29bb84bf665d3915c8fd6db1f9561f
fady emil <fadymobiledev@gmail.com>
2025-01-30 19:17:36 +0200

    Add signup form fields and sign-up button widgets
    Implemented `FieldPassword`, `FieldName`, and `FieldEmail` widgets for the signup form. Also, added the `SignUpButton` widget which handles the sign-up process, including form validation and API interaction.

---
+++ lib/features/auth/presentation/view/signup/widget/field_sigin_up.dart
@@ -0,0 +1,77 @@
+import 'package:e_commerce/core/widget/app_text_form_field.dart';
+import 'package:flutter/material.dart';
+
+class FieldPassword extends StatelessWidget {
+  const FieldPassword({
+    super.key,
+    required this.passwordController,
+    required this.isObscureText,
+    required this.onObscureTextChanged,
+  });
+
+  final TextEditingController passwordController;
+  final bool isObscureText;
+  final ValueChanged<bool> onObscureTextChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppTextFormField(
+      hintText: 'كلمة المرور',
+      validator: (value) =>
+          value == null || value.isEmpty ? 'رجاء ادخال كلمة المرور' : null,
+      controller: passwordController,
+      isObscureText: isObscureText,
+      keyboardType: TextInputType.visiblePassword,
+      maxlines: 1,
+      suffixIcon: GestureDetector(
+        onTap: () {
+          onObscureTextChanged(!isObscureText); // Toggle visibility
+        },
+        child: Icon(
+          isObscureText ? Icons.visibility_off : Icons.visibility,
+          color: const Color(0xffC9CECF),
+        ),
+      ),
+    );
+  }
+}
+
+class FieldName extends StatelessWidget {
+  const FieldName({
+    super.key,
+    required this.nameController,
+  });
+
+  final TextEditingController nameController;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppTextFormField(
+      hintText: 'الاسم كامل',
+      validator: (value) =>
+          value == null || value.isEmpty ? 'رجاء ادخال الاسم كامل' : null,
+      controller: nameController,
+    );
+  }
+}
+
+class FieldEmail extends StatelessWidget {
+  const FieldEmail({
+    super.key,
+    required this.emailController,
+  });
+
+  final TextEditingController emailController;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppTextFormField(
+      hintText: 'البريد الإلكتروني',
+      keyboardType: TextInputType.emailAddress,
+      validator: (value) => value == null || value.isEmpty
+          ? 'رجاء ادخال البريد الإلكتروني'
+          : null,
+      controller: emailController,
+    );
+  }
+}
---
+++ lib/features/auth/presentation/view/signup/widget/sign_up_button.dart
@@ -0,0 +1,86 @@
+import 'package:e_commerce/core/helper_functions/build_error_message.dart';
+import 'package:e_commerce/core/helper_functions/router/router_name.dart';
+import 'package:e_commerce/core/theme/app_colors.dart';
+import 'package:e_commerce/core/theme/app_text_styles.dart';
+import 'package:e_commerce/core/widget/cusstom_button.dart';
+import 'package:e_commerce/features/auth/data/model/user_model_api.dart';
+import 'package:e_commerce/features/auth/presentation/manger/sign_up/register_using_api_riverpod.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+class SignUpButton extends ConsumerStatefulWidget {
+  const SignUpButton({
+    super.key,
+    required this.isTermsAccepted,
+    required this.nameController,
+    required this.emailController,
+    required this.passwordController,
+    required this.formKey,
+    required this.autovalidateMode,
+    required this.updateAutovalidateMode,
+  });
+
+  final bool isTermsAccepted;
+  final TextEditingController nameController;
+  final TextEditingController emailController;
+  final TextEditingController passwordController;
+  final GlobalKey<FormState> formKey;
+  final AutovalidateMode autovalidateMode;
+  final Function(AutovalidateMode) updateAutovalidateMode;
+
+  @override
+  ConsumerState<SignUpButton> createState() => _SignUpButtonState();
+}
+
+class _SignUpButtonState extends ConsumerState<SignUpButton> {
+  bool isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return CusstomButton(
+      buttonText: isLoading ? 'جارٍ التسجيل...' : 'إنشاء حساب جديد',
+      textStyle: AppTextStyles.bodyBasaBold16,
+      onPressed: widget.isTermsAccepted
+          ? () async {
+              if (widget.formKey.currentState!.validate()) {
+                setState(() {
+                  isLoading = true;
+                });
+                final userModel = UserModelApi(
+                  name: widget.nameController.text,
+                  email: widget.emailController.text,
+                  password: widget.passwordController.text,
+                  image: '',
+                  phone: '09978784503',
+                );
+
+                final result =
+                    await ref.read(registerUsingApiProvider(userModel).future);
+
+                result.fold(
+                  (failure) {
+                    buildErrorBar(context, failure.toString());
+                  },
+                  (success) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('تم التسجيل بنجاح')),
+                    );
+                    context.goNamed(RouterName.login);
+                  },
+                );
+
+                setState(() {
+                  isLoading = false;
+                });
+              } else {
+                widget.updateAutovalidateMode(AutovalidateMode.always);
+              }
+            }
+          : () {
+              buildErrorBar(context, 'يجب أن تقبل الشروط والأحكام');
+            },
+      backgroundColor: widget.isTermsAccepted ? null : AppColors.grayscale300,
+    );
+  }
+}
