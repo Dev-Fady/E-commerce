88f5e16e9db72e21e6899895dbcf026e27104693
fady emil <fadymobiledev@gmail.com>
2025-01-30 15:03:37 +0200

    Add CategoriesModel and update CategoriesRepoImpl
    Introduce `CategoriesModel` and its associated classes (`Data` and `Datum`) with JSON serialization support. Update `CategoriesRepoImpl` to use the new `ApiService` and `CategoriesModel`. Remove the old `ModelCategories` file.

---
+++ lib/features/home/data/categories_model.dart
@@ -0,0 +1,84 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:e_commerce/features/home/domain/categories_entity.dart';
+
+part 'categories_model.g.dart';
+
+@JsonSerializable()
+class CategoriesModel {
+  bool status;
+  dynamic message;
+  Data data;
+
+  CategoriesModel({
+    required this.status,
+    required this.message,
+    required this.data,
+  });
+
+  // fromJson method
+  factory CategoriesModel.fromJson(Map<String, dynamic> json) =>
+      _$CategoriesModelFromJson(json);
+
+  // toJson method
+  Map<String, dynamic> toJson() => _$CategoriesModelToJson(this);
+}
+
+@JsonSerializable()
+class Data {
+  int currentPage;
+  List<Datum> data;
+  String firstPageUrl;
+  int from;
+  int lastPage;
+  String lastPageUrl;
+  dynamic nextPageUrl;
+  String path;
+  int perPage;
+  dynamic prevPageUrl;
+  int to;
+  int total;
+
+  Data({
+    required this.currentPage,
+    required this.data,
+    required this.firstPageUrl,
+    required this.from,
+    required this.lastPage,
+    required this.lastPageUrl,
+    required this.nextPageUrl,
+    required this.path,
+    required this.perPage,
+    required this.prevPageUrl,
+    required this.to,
+    required this.total,
+  });
+
+  // fromJson method
+  factory Data.fromJson(Map<String, dynamic> json) => _$DataFromJson(json);
+
+  // toJson method
+  Map<String, dynamic> toJson() => _$DataToJson(this);
+}
+
+@JsonSerializable()
+class Datum extends CategoriesEntity {
+  int id;
+  String name;
+  String image;
+
+  Datum({
+    required this.id,
+    required this.name,
+    required this.image,
+  }) : super(
+          id: id,
+          name: name,
+          image: image,
+        );
+
+  // fromJson method
+  factory Datum.fromJson(Map<String, dynamic> json) => _$DatumFromJson(json);
+
+  // toJson method
+  Map<String, dynamic> toJson() => _$DatumToJson(this);
+}
--- lib/features/home/data/categories_repo_impl.dart
+++ lib/features/home/data/categories_repo_impl.dart
@@ -4,26 +4,24 @@ import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 import 'package:e_commerce/core/errors/exceptions.dart';
 import 'package:e_commerce/core/errors/faliures.dart';
-import 'package:e_commerce/core/services/api/api_comsumer.dart';
-import 'package:e_commerce/core/services/api/end_ponits.dart';
-import 'package:e_commerce/features/home/data/model_categories.dart';
+import 'package:e_commerce/core/services/api/api_service.dart';
+import 'package:e_commerce/features/home/data/categories_model.dart';
 import 'package:e_commerce/features/home/domain/categories_entity.dart';
 import 'package:e_commerce/features/home/domain/categories_repo.dart';
 
 class CategoriesRepoImpl extends CategoriesRepo {
-  final ApiConsumer apiConsumer;
+  final ApiService apiService;
 
-  CategoriesRepoImpl({required this.apiConsumer});
+  CategoriesRepoImpl({required this.apiService});
 
   @override
   Future<Either<Faliure, List<CategoriesEntity>>> getCategories() async {
     try {
-      final data = await apiConsumer.get(EndPonits.categories);
+      final  data = await apiService.getCategories();
 
-      log('API Response: $data'); // Debugging line
+      log('API Response: $data');
 
-      final List<dynamic> results =
-          data['data']['data']; // Ensure this matches API response
+      final List<dynamic> results = data['data']['data'];
       final categories = results.map((e) => Datum.fromJson(e)).toList();
 
       return Right(categories);
