e6f31286ec3ffdca5b9d1213b753bea2320515ae
fady emil <fadymobiledev@gmail.com>
2025-01-30 19:13:36 +0200

    Add register endpoint to ApiService
    Implemented the register endpoint in ApiService to handle user registration. Added the necessary import for UserModelApi and updated the generated file to include the register method.

--- lib/core/services/api/api_service.dart
+++ lib/core/services/api/api_service.dart
@@ -1,18 +1,16 @@
 import 'package:dio/dio.dart';
+import 'package:e_commerce/features/auth/data/model/user_model_api.dart';
 import 'package:retrofit/retrofit.dart';
 
-part 'api_service.g.dart'; 
+part 'api_service.g.dart';
 
 @RestApi(baseUrl: 'https://student.valuxapps.com/api/')
 abstract class ApiService {
   factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
 
   @GET("categories")
-  Future<dynamic> getCategories(); 
+  Future<dynamic> getCategories();
 
-  // @GET("/users/{id}")
-  // Future<UserModel> getUser(@Path("id") int id); // جلب مستخدم معين
-
-  // @POST("/users")
-  // Future<UserModel> createUser(@Body() UserModel user); // إنشاء مستخدم جديد
+  @POST("register")
+  Future<dynamic> register(@Body() UserModelApi userModelApi);
 }
