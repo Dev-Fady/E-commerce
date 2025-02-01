77be6f2e9b5c28277062ae5804a202df437389fc
fady emil <fadymobiledev@gmail.com>
2025-02-01 19:16:25 +0200

    Add `getCategoriesDetails` method to `CategoriesRepoImpl`
    Implemented the `getCategoriesDetails` method in `CategoriesRepoImpl` to fetch category details using the provided token and category ID. Added necessary imports for `CategoriesDetailsModel` and `CategoriesDetailsEntity`.

--- lib/features/home/data/categories_repo_impl.dart
+++ lib/features/home/data/categories_repo_impl.dart
@@ -5,7 +5,9 @@ import 'package:dio/dio.dart';
 import 'package:e_commerce/core/errors/exceptions.dart';
 import 'package:e_commerce/core/errors/faliures.dart';
 import 'package:e_commerce/core/services/api/api_service.dart';
+import 'package:e_commerce/features/home/data/categories_details_model.dart';
 import 'package:e_commerce/features/home/data/categories_model.dart';
+import 'package:e_commerce/features/home/domain/categories_details_entity.dart';
 import 'package:e_commerce/features/home/domain/categories_entity.dart';
 import 'package:e_commerce/features/home/domain/categories_repo.dart';
 
@@ -17,7 +19,7 @@ class CategoriesRepoImpl extends CategoriesRepo {
   @override
   Future<Either<Faliure, List<CategoriesEntity>>> getCategories() async {
     try {
-      final  data = await apiService.getCategories();
+      final data = await apiService.getCategories();
 
       log('API Response: $data');
 
@@ -31,6 +33,21 @@ class CategoriesRepoImpl extends CategoriesRepo {
     } catch (e) {
       log('Exception in getCategories: $e');
       throw Left(CustomException(message: 'حدث خطأ أثناء جلب البيانات.'));
+    }
+  }
+
+  @override
+  Future<Either<Faliure, List<CategoriesDetailsEntity>>> getCategoriesDetails(
+      {required String token, required int categoryId}) async {
+    try {
+      final data = await apiService.categoryDetails(token, categoryId);
+      log('API Response: $data');
+      final List<dynamic> results = data['data']['data'];
+      final products = results.map((e) => DataPro.fromJson(e)).toList();
+      return Right(products);
+    } catch (e) {
+      log('Exception in getCategoriesDetails: $e');
+      throw Left(CustomException(message: 'حدث خطأ أثنا�� ��لب البيانات.'));
     }
   }
 }
