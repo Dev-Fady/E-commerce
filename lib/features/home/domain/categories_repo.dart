2f323246d98b862e59a6ac407e001d36a867616d
fady emil <fadymobiledev@gmail.com>
2025-02-01 19:15:42 +0200

    Add CategoriesDetailsEntity and update CategoriesRepo
    Introduce a new entity `CategoriesDetailsEntity` to represent detailed category information. Update `CategoriesRepo` to include a method `getCategoriesDetails` that fetches detailed category information based on a token and category ID.

--- lib/features/home/domain/categories_repo.dart
+++ lib/features/home/domain/categories_repo.dart
@@ -1,7 +1,11 @@
 import 'package:dartz/dartz.dart';
 import 'package:e_commerce/core/errors/faliures.dart';
+import 'package:e_commerce/features/home/domain/categories_details_entity.dart';
 import 'package:e_commerce/features/home/domain/categories_entity.dart';
 
 abstract class CategoriesRepo {
   Future<Either<Faliure, List<CategoriesEntity>>> getCategories();
+
+  Future<Either<Faliure, List<CategoriesDetailsEntity>>> getCategoriesDetails(
+      {required String token,required int categoryId});
 }
