da5f794076b11af3d13250b398a6b0898c78ee7e
fady emil <fadymobiledev@gmail.com>
2025-02-02 16:17:40 +0200

    Update router paths and imports for categories details and all products
    Updated the router paths and imports to correctly reflect the new categories details and all products views. Adjusted the `router.dart` file to include the new routes and their corresponding builders. Also, corrected the import paths for the `categories_details_entity` and the new views. Updated the `router_name.dart` file to include the new route names for the categories details and all products views.

--- lib/core/helper_functions/router/router.dart
+++ lib/core/helper_functions/router/router.dart
@@ -5,9 +5,8 @@ import 'package:e_commerce/features/auth/presentation/manger/sign_up/sign_up_cub
 import 'package:e_commerce/features/auth/presentation/view/login/login_view.dart';
 import 'package:e_commerce/features/auth/presentation/view/signup/sign_up_view.dart';
 import 'package:e_commerce/features/best_selling_fruits/presentation/view/bset_selling_view.dart';
-// import 'package:e_commerce/features/home/domain/categories_entity.dart';
+// import 'package:e_commerce/features/home/domain/entites/categories_entity.dart';
 import 'package:e_commerce/features/home/presentation/view/home_view.dart';
-import 'package:e_commerce/features/home/presentation/view/productis_screen/categories_details_view.dart';
 import 'package:e_commerce/features/main/presentation/view/main_view.dart';
 import 'package:e_commerce/features/on_boarding/presentation/view/on_boarding_view.dart';
 import 'package:e_commerce/features/splash/presentation/views/splash_view.dart';
@@ -15,6 +14,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../../features/auth/domain/repo/auth_repo.dart';
+import '../../../features/home/domain/entites/categories_details_entity.dart';
+import '../../../features/home/presentation/view/productis_screen/categories_details_rivepod_work.dart';
+import '../../../features/home/presentation/view/productis_screen/widget/all_productis.dart';
 
 GoRouter createRouter(String initialLocation) {
   return GoRouter(
@@ -26,11 +28,22 @@ GoRouter createRouter(String initialLocation) {
         builder: (context, state) => const SplashView(),
       ),
       GoRoute(
-          path: RouterName.products_view,
-          name: RouterName.products_view,
+          path: RouterName.categories_details_rivepod_work,
+          name: RouterName.categories_details_rivepod_work,
           builder: (context, state) {
-            // final category = state.extra as CategoriesEntity;
-            return CategoriesDetailsView();
+            final int category = state.extra as int;
+            return CategoriesDetailsRivepodWork(
+              categoryId: category,
+            );
+          }),
+      GoRoute(
+          path: RouterName.all_productis,
+          name: RouterName.all_productis,
+          builder: (context, state) {
+            final  categoriesDtails = state.extra as List<CategoriesDetailsEntity>;
+            return AllProductis(
+              data: categoriesDtails,
+            );
           }),
       GoRoute(
         path: RouterName.onBoarding,
--- lib/core/helper_functions/router/router_name.dart
+++ lib/core/helper_functions/router/router_name.dart
@@ -2,9 +2,10 @@ class RouterName {
   static const String splashScreen = "/SplashScreen";
   static const String onBoarding = "/onBoarding";
   static const String login = "/login";
-  static const String signUP="/signup";
+  static const String signUP = "/signup";
   static const String home = "/home";
   static const String main_view = "/main_view";
-  static const String bestSelling="/bestSelling";
-  static const String products_view="/products_view";
+  static const String bestSelling = "/bestSelling";
+  static const String categories_details_rivepod_work = "/products_view";
+  static const String all_productis = "/all_Productis";
 }
