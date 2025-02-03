20ed5dca6bf51fa3ab1cec7d48087f166f9a4ea1
fady emil <fadymobiledev@gmail.com>
2025-02-03 12:49:22 +0200

    Add navigation to product details on item tap

--- lib/features/home/presentation/view/productis_screen/widget/BestSeller.dart
+++ lib/features/home/presentation/view/productis_screen/widget/BestSeller.dart
@@ -1,7 +1,9 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:e_commerce/core/helper_functions/router/router_name.dart';
 import 'package:e_commerce/core/theme/app_text_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
 import 'package:remixicon/remixicon.dart';
 
 import '../../../../domain/entites/categories_details_entity.dart';
@@ -13,59 +15,64 @@ class BestSeller extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 155.w,
-      padding: EdgeInsets.all(12.r),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(20.r),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black12,
-            blurRadius: 8,
-            spreadRadius: 2,
-          ),
-        ],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(12.r),
-            child: CachedNetworkImage(
-              width: double.infinity,
-              height: 120.h,
-              fit: BoxFit.fill,
-              imageUrl: data.image,
-              placeholder: (context, url) =>
-                  Center(child: CircularProgressIndicator()),
-              errorWidget: (context, url, error) => Image.asset(
-                  'assets/images/customer/avatar.png',
-                  fit: BoxFit.cover),
+    return InkWell(
+      onTap: () {
+        context.push(RouterName.product_details_view, extra: data);
+      },
+      child: Container(
+        width: 155.w,
+        padding: EdgeInsets.all(12.r),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20.r),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 8,
+              spreadRadius: 2,
             ),
-          ),
-          SizedBox(height: 8.h),
-          Text(
-            data.name,
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis,
-            style: AppTextStyles.bodySmallSemiBold13,
-          ),
-          SizedBox(height: 4.h),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                '\$ ${data.price}',
-                style: AppTextStyles.bodyBasaBold16,
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12.r),
+              child: CachedNetworkImage(
+                width: double.infinity,
+                height: 120.h,
+                fit: BoxFit.fill,
+                imageUrl: data.image,
+                placeholder: (context, url) =>
+                    Center(child: CircularProgressIndicator()),
+                errorWidget: (context, url, error) => Image.asset(
+                    'assets/images/customer/avatar.png',
+                    fit: BoxFit.cover),
               ),
-              IconButton(
-                icon: const Icon(Remix.heart_3_line),
-                onPressed: () {},
-              ),
-            ],
-          ),
-        ],
+            ),
+            SizedBox(height: 8.h),
+            Text(
+              data.name,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: AppTextStyles.bodySmallSemiBold13,
+            ),
+            SizedBox(height: 4.h),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '\$ ${data.price}',
+                  style: AppTextStyles.bodyBasaBold16,
+                ),
+                IconButton(
+                  icon: const Icon(Remix.heart_3_line),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/features/home/presentation/view/productis_screen/widget/build_all_item.dart
+++ lib/features/home/presentation/view/productis_screen/widget/build_all_item.dart
@@ -1,7 +1,11 @@
+import 'dart:developer';
+
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:e_commerce/core/helper_functions/router/router_name.dart';
 import 'package:e_commerce/features/home/domain/entites/categories_details_entity.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
 
 class BuildAllItem extends StatelessWidget {
   const BuildAllItem({super.key, required this.data});
@@ -9,68 +13,74 @@ class BuildAllItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card.outlined(
-      child: Container(
-        decoration: BoxDecoration(
-          // color: Colors.white,
-          borderRadius: BorderRadius.circular(12.r),
-        ),
-        child: Padding(
-          padding: EdgeInsets.all(12.w),
-          child: Row(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(12.r),
-                  bottomLeft: Radius.circular(12.r),
+    return InkWell(
+      onTap: () {
+        context.push(RouterName.product_details_view, extra: data);
+        log(data.oldPrice.toString());
+      },
+      child: Card.outlined(
+        child: Container(
+          decoration: BoxDecoration(
+            // color: Colors.white,
+            borderRadius: BorderRadius.circular(12.r),
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(12.w),
+            child: Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(12.r),
+                    bottomLeft: Radius.circular(12.r),
+                  ),
+                  child: CachedNetworkImage(
+                    width: 70.w,
+                    height: 70.h,
+                    fit: BoxFit.cover,
+                    imageUrl: data.image,
+                    placeholder: (context, url) =>
+                        Center(child: CircularProgressIndicator()),
+                    errorWidget: (context, url, error) => Image.asset(
+                        'assets/images/customer/avatar.png',
+                        fit: BoxFit.cover),
+                  ),
                 ),
-                child: CachedNetworkImage(
-                  width: 70.w,
-                  height: 70.h,
-                  fit: BoxFit.cover,
-                  imageUrl: data.image,
-                  placeholder: (context, url) =>
-                      Center(child: CircularProgressIndicator()),
-                  errorWidget: (context, url, error) => Image.asset(
-                      'assets/images/customer/avatar.png',
-                      fit: BoxFit.cover),
-                ),
-              ),
-              SizedBox(width: 12.w),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      data.name,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16.sp,
+                SizedBox(width: 12.w),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        data.name,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16.sp,
+                        ),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
                       ),
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    SizedBox(height: 4.h),
-                    Text(
-                      data.description,
-                      style:
-                          TextStyle(color: Colors.grey[600], fontSize: 14.sp),
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    SizedBox(height: 8.h),
-                    Text(
-                      "L.E ${data.price}",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18.sp,
+                      SizedBox(height: 4.h),
+                      Text(
+                        data.description,
+                        style:
+                            TextStyle(color: Colors.grey[600], fontSize: 14.sp),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
                       ),
-                    ),
-                  ],
+                      SizedBox(height: 8.h),
+                      Text(
+                        "L.E ${data.price}",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.sp,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              Icon(Icons.favorite_border, size: 24.sp),
-            ],
+                Icon(Icons.favorite_border, size: 24.sp),
+              ],
+            ),
           ),
         ),
       ),
