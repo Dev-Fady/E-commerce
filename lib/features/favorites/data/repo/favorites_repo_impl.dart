40f3f4de6835837bc1cdab07e10af76d5cc5df92
fady emil <fadymobiledev@gmail.com>
2025-02-05 16:19:26 +0200

    Add deleteFavorite method to FavoritesRepo and implementation

--- lib/features/favorites/data/repo/favorites_repo_impl.dart
+++ lib/features/favorites/data/repo/favorites_repo_impl.dart
@@ -5,8 +5,10 @@ import 'package:e_commerce/core/errors/exceptions.dart';
 import 'package:e_commerce/core/errors/faliures.dart';
 import 'package:e_commerce/core/services/api/api_service.dart';
 import 'package:e_commerce/features/favorites/data/models/add_or_delete_favoraite_model.dart';
+import 'package:e_commerce/features/favorites/data/models/delete_favorite_model.dart';
 import 'package:e_commerce/features/favorites/data/models/get_favorites_model.dart';
 import 'package:e_commerce/features/favorites/domain/entites/add_or_delete_favoraite_entity.dart';
+import 'package:e_commerce/features/favorites/domain/entites/delete_favoraite_Entitty.dart';
 import 'package:e_commerce/features/favorites/domain/entites/get_favorites_entity.dart';
 import 'package:e_commerce/features/favorites/domain/repo/favorites_repo.dart';
 
@@ -51,6 +53,19 @@ class FavoritesRepoImpl implements FavoritesRepo {
     } catch (e) {
       log('Exception in GetFavoritesEntity: $e');
       throw Left(CustomException(message: 'حدث خطأ أثناء طلب البيانات.'));
+    }
+  }
+
+  @override
+  Future<Either<Faliure, DeleteFavoraiteEntitty>> deleteFavorite({required String token, required int productId}) async{
+    try {
+      final response = await apiService.deleteFavorait(token, productId);
+      final deleteFavoriteModel = DeleteFavoriteModel.fromJson(response);
+      final deleteFavoriteEntity = deleteFavoriteModel.toEntity();
+      return Right(deleteFavoriteEntity);
+    } catch (e) {
+      log('Exception in deleteFavorite: $e');
+      return Left(ServerFaliure("حدث خطأ ما. الرجا�� المحاولة مرة ا��خرى. "));
     }
   }
 }
--- lib/features/favorites/domain/repo/favorites_repo.dart
+++ lib/features/favorites/domain/repo/favorites_repo.dart
@@ -1,10 +1,14 @@
 import 'package:dartz/dartz.dart';
 import 'package:e_commerce/core/errors/faliures.dart';
 import 'package:e_commerce/features/favorites/domain/entites/add_or_delete_favoraite_entity.dart';
+import 'package:e_commerce/features/favorites/domain/entites/delete_favoraite_Entitty.dart';
 import 'package:e_commerce/features/favorites/domain/entites/get_favorites_entity.dart';
 
 abstract class FavoritesRepo {
   Future<Either<Faliure, AddOrDeleteFavoraiteEntity>> addOrRemoveFavorait(
+      {required String token, required int productId});
+
+  Future<Either<Faliure, DeleteFavoraiteEntitty>> deleteFavorite(
       {required String token, required int productId});
 
   Future<Either<Faliure, List<GetFavoritesEntity>>> getFavoritesEntity(
