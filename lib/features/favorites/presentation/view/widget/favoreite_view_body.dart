100962a227aacc745da167ae538f9d01492648a6
fady emil <fadymobiledev@gmail.com>
2025-02-05 17:33:49 +0200

    Convert `FavoreiteViewBody` to `ConsumerStatefulWidget` and refresh favorites

--- lib/features/favorites/presentation/view/widget/favoreite_view_body.dart
+++ lib/features/favorites/presentation/view/widget/favoreite_view_body.dart
@@ -1,20 +1,32 @@
 import 'package:e_commerce/features/favorites/domain/entites/get_favorites_entity.dart';
+import 'package:e_commerce/features/favorites/presentation/manger/get_favorite_rivvepod.dart';
 import 'package:e_commerce/features/favorites/presentation/view/widget/favorite_item_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FavoreiteViewBody extends StatelessWidget {
-  const FavoreiteViewBody({
-    super.key,
-    required this.data,
-  });
-
+class FavoreiteViewBody extends ConsumerStatefulWidget {
+  const FavoreiteViewBody({super.key, required this.data});
   final List<GetFavoritesEntity> data;
+
+  @override
+  ConsumerState<FavoreiteViewBody> createState() => _FavoreiteViewBodyState();
+}
+
+class _FavoreiteViewBodyState extends ConsumerState<FavoreiteViewBody> {
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    // ignore: unused_result
+    ref.refresh(getFavoritesProvider);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: GridView.builder(
-        itemCount: data.length,
+        itemCount: widget.data.length,
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 0.59,
@@ -23,7 +35,7 @@ class FavoreiteViewBody extends StatelessWidget {
         ),
         itemBuilder: (context, index) {
           return FavoriteItemCard(
-            data: data[index],
+            data: widget.data[index],
           );
         },
       ),
