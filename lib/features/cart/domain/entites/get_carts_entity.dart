7f34707f3c457c2d703ee002bb6727cd48dbe564
fady emil <fadymobiledev@gmail.com>
2025-02-06 19:11:23 +0200

    Add GetCartsEntity and update CartRepo with getCarts method

---
+++ lib/features/cart/domain/entites/get_carts_entity.dart
@@ -0,0 +1,23 @@
+class GetCartsEntity {
+  final int mainId; // ID الأساسي للمفضلة
+  final int productId; // ID الخاص بالمنتج
+  final int quantity;
+  final int price;
+  final int oldPrice;
+  final int discount;
+  final String image;
+  final String name;
+  final String description;
+
+  GetCartsEntity({
+    required this.mainId,
+    required this.productId,
+    required this.quantity,
+    required this.price,
+    required this.oldPrice,
+    required this.discount,
+    required this.image,
+    required this.name,
+    required this.description,
+  });
+}
--- lib/features/cart/domain/repo/cart_repo.dart
+++ lib/features/cart/domain/repo/cart_repo.dart
@@ -1,9 +1,12 @@
 import 'package:dartz/dartz.dart';
 import 'package:e_commerce/core/errors/faliures.dart';
 import 'package:e_commerce/features/cart/domain/entites/add_or_remove_cart_entity.dart';
+import 'package:e_commerce/features/cart/domain/entites/get_carts_entity.dart';
 
 abstract class CartRepo {
-  Future<Either<Faliure,AddOrRemoveCartEntity >> addOrRemoveCart(
+  Future<Either<Faliure, AddOrRemoveCartEntity>> addOrRemoveCart(
       {required String token, required int productId});
 
+  Future<Either<Faliure, List<GetCartsEntity>>> getCarts(
+      {required String token});
 }
