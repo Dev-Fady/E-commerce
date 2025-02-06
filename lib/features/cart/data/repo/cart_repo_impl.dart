a6d13bf503ad7b63549e06601c00b67ed34e8eff
fady emil <fadymobiledev@gmail.com>
2025-02-06 19:11:45 +0200

    Add getCarts method to CartRepoImpl

--- lib/features/cart/data/repo/cart_repo_impl.dart
+++ lib/features/cart/data/repo/cart_repo_impl.dart
@@ -1,10 +1,13 @@
 import 'dart:developer';
 
 import 'package:dartz/dartz.dart';
+import 'package:e_commerce/core/errors/exceptions.dart';
 import 'package:e_commerce/core/errors/faliures.dart';
 import 'package:e_commerce/core/services/api/api_service.dart';
 import 'package:e_commerce/features/cart/data/model/add_or_remove_cart_model.dart';
+import 'package:e_commerce/features/cart/data/model/get_carts_model.dart';
 import 'package:e_commerce/features/cart/domain/entites/add_or_remove_cart_entity.dart';
+import 'package:e_commerce/features/cart/domain/entites/get_carts_entity.dart';
 import 'package:e_commerce/features/cart/domain/repo/cart_repo.dart';
 
 class CartRepoImpl implements CartRepo {
@@ -28,6 +31,24 @@ class CartRepoImpl implements CartRepo {
     } catch (e) {
       log('Exception in addOrRemoveCart: $e');
       return Left(ServerFaliure(" حدث خطأ ما. الرجاء المحاولة مرة أخرى. "));
+    }
+  }
+
+  @override
+  Future<Either<Faliure, List<GetCartsEntity>>> getCarts(
+      {required String token}) async {
+    try {
+      final data = await apiService.getCart(token);
+      log('API Response: $data');
+
+      final List<dynamic> results = data['data']['cart_items'];
+
+      final products = results.map((item) => CartItems.fromJson(item)).toList();
+
+      return Right(products);
+    } catch (e) {
+      log('Exception in GetFavoritesEntity: $e');
+      throw Left(CustomException(message: 'حدث خطأ أثناء طلب البيانات.'));
     }
   }
 }
