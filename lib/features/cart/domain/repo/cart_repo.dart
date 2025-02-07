66bc99b4bf609728c3a66e3bb9f35047ba435fae
fady emil <fadymobiledev@gmail.com>
2025-02-07 15:46:40 +0200

    Add deleteCart functionality to CartRepo and CartRepoImpl

--- lib/features/cart/domain/repo/cart_repo.dart
+++ lib/features/cart/domain/repo/cart_repo.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:e_commerce/core/errors/faliures.dart';
 import 'package:e_commerce/features/cart/domain/entites/add_or_remove_cart_entity.dart';
+import 'package:e_commerce/features/cart/domain/entites/delete_cart_entity.dart';
 import 'package:e_commerce/features/cart/domain/entites/get_carts_entity.dart';
 
 abstract class CartRepo {
@@ -9,4 +10,7 @@ abstract class CartRepo {
 
   Future<Either<Faliure, List<GetCartsEntity>>> getCarts(
       {required String token});
+
+  Future<Either<Faliure, DeleteCartEntity>> deleteCart(
+      {required String token, required int productId});
 }
