3260e61d0a05bca9d7511f9fec0cebc12d01daab
fady emil <fadymobiledev@gmail.com>
2025-02-07 15:47:13 +0200

    Integrate DeleteCartCubit and handle delete functionality in CartItem

--- lib/features/cart/presentation/view/widget/cart_item.dart
+++ lib/features/cart/presentation/view/widget/cart_item.dart
@@ -1,10 +1,15 @@
+import 'package:e_commerce/core/DI/dependency_injection.dart';
+import 'package:e_commerce/core/services/api/api_service.dart';
+import 'package:e_commerce/features/cart/data/repo/cart_repo_impl.dart';
 import 'package:e_commerce/features/cart/domain/entites/get_carts_entity.dart';
+import 'package:e_commerce/features/cart/presentation/manger/cubit/delete_cart_cubit.dart';
 import 'package:e_commerce/features/cart/presentation/view/widget/delete_cart.dart';
 import 'package:e_commerce/features/cart/presentation/view/widget/image_protuction.dart';
 import 'package:e_commerce/features/cart/presentation/view/widget/name_prodcution.dart';
 import 'package:e_commerce/features/cart/presentation/view/widget/price_protuction.dart';
 import 'package:e_commerce/features/cart/presentation/view/widget/quantity.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CartItem extends StatelessWidget {
   const CartItem({
@@ -15,30 +20,34 @@ class CartItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.only(bottom: 16.0),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      elevation: 4,
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Row(
-          children: [
-            ImageProtuction(data: data),
-            const SizedBox(width: 12),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  NameProdcution(name: data.name),
-                  const SizedBox(height: 4),
-                  PriceProtuction(price: data.price),
-                  const SizedBox(height: 8),
-                  Quantity(data: data),
-                ],
+    return BlocProvider(
+      create: (context) => DeleteCartCubit(
+          cartRepo: CartRepoImpl(apiService: getIt<ApiService>())),
+      child: Card(
+        margin: const EdgeInsets.only(bottom: 16.0),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        elevation: 4,
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Row(
+            children: [
+              ImageProtuction(data: data),
+              const SizedBox(width: 12),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    NameProdcution(name: data.name),
+                    const SizedBox(height: 4),
+                    PriceProtuction(price: data.price),
+                    const SizedBox(height: 8),
+                    Quantity(data: data),
+                  ],
+                ),
               ),
-            ),
-            const DeleteCart(),
-          ],
+              DeleteCart(data: data),
+            ],
+          ),
         ),
       ),
     );
--- lib/features/cart/presentation/view/widget/delete_cart.dart
+++ lib/features/cart/presentation/view/widget/delete_cart.dart
@@ -1,15 +1,50 @@
+import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
+import 'package:e_commerce/core/widget/custom_snackbar.dart';
+import 'package:e_commerce/features/cart/domain/entites/get_carts_entity.dart';
+import 'package:e_commerce/features/cart/presentation/manger/cubit/delete_cart_cubit.dart';
+import 'package:e_commerce/features/cart/presentation/manger/cubit/delete_cart_state.dart';
+import 'package:e_commerce/features/cart/presentation/manger/get_cart_rivepod.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class DeleteCart extends StatelessWidget {
-  const DeleteCart({
-    super.key,
-  });
+class DeleteCart extends ConsumerWidget {
+  const DeleteCart({super.key, required this.data});
+
+  final GetCartsEntity data;
 
   @override
-  Widget build(BuildContext context) {
-    return IconButton(
-      icon: const Icon(Icons.delete, color: Colors.red),
-      onPressed: () {}, // Handle remove item
+  Widget build(BuildContext context, WidgetRef ref) {
+    return BlocConsumer<DeleteCartCubit, DeleteCartState>(
+      listener: (context, state) {
+        if (state is DeleteCartFailure) {
+          CustomSnackbar.show(
+            context: context,
+            title: 'تم بنجاح!',
+            message: state.error.message,
+            type: ContentType.failure,
+          );
+        } else if (state is DeleteCartSuccess) {
+          CustomSnackbar.show(
+            context: context,
+            title: 'تم بنجاح!',
+            message: state.Cart.message,
+            type: ContentType.success,
+          );
+          ref.invalidate(getCartRepoProvider);
+        }
+      },
+      builder: (context, state) {
+        if (state is DeleteCartLoading) {
+          return Center(child: CircularProgressIndicator());
+        }
+        return IconButton(
+          icon: const Icon(Icons.delete, color: Colors.red),
+          onPressed: () async {
+            await context.read<DeleteCartCubit>().deleteFavorite(data.mainId);
+          },
+        );
+      },
     );
   }
 }
