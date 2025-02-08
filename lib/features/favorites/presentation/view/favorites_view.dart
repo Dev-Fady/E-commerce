4b9217b887d5020908cadc2578e76b10bb8e47ac
fady emil <fadymobiledev@gmail.com>
2025-02-08 16:49:38 +0200

    refres favorites view if add item first time

--- lib/features/favorites/presentation/view/favorites_view.dart
+++ lib/features/favorites/presentation/view/favorites_view.dart
@@ -1,12 +1,29 @@
 import 'package:e_commerce/features/favorites/domain/entites/get_favorites_entity.dart';
 import 'package:e_commerce/features/favorites/presentation/view/widget/NoItem.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../manger/get_favorite_rivvepod.dart';
 import 'widget/favoreite_view_body.dart';
 
-class FavoritesView extends StatelessWidget {
+class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key, required this.data});
   final List<GetFavoritesEntity> data;
+
+  @override
+  ConsumerState<FavoritesView> createState() => _FavoritesViewState();
+}
+
+class _FavoritesViewState extends ConsumerState<FavoritesView> {
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    Future.microtask(() {
+      // ignore: unused_result
+      ref.refresh(getFavoritesProvider);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,9 +34,9 @@ class FavoritesView extends StatelessWidget {
         title: const Text("Favorites"),
         centerTitle: true,
       ),
-      body: data.isEmpty
+      body: widget.data.isEmpty
           ? NoItem(text: "No favorites yet!")
-          : FavoreiteViewBody(data: data),
+          : FavoreiteViewBody(data: widget.data),
     );
   }
 }
