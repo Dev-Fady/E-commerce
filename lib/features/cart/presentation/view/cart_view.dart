6671cbdb3284100c48fa480054f9c7c3720a8468
fady emil <fadymobiledev@gmail.com>
2025-02-08 16:27:21 +0200

    refresh if no item and add item to cart view

--- lib/features/cart/presentation/view/cart_view.dart
+++ lib/features/cart/presentation/view/cart_view.dart
@@ -1,13 +1,30 @@
 import 'package:e_commerce/features/cart/domain/entites/get_carts_entity.dart';
 import 'package:e_commerce/features/favorites/presentation/view/widget/NoItem.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../manger/get_cart_rivepod.dart';
 import 'widget/cart_view_body.dart';
 import 'widget/cart_view_botton_navigation_bar.dart';
 
-class CartView extends StatelessWidget {
+class CartView extends ConsumerStatefulWidget {
   const CartView({super.key, required this.data});
   final List<GetCartsEntity> data;
+
+  @override
+  ConsumerState<CartView> createState() => _CartViewState();
+}
+
+class _CartViewState extends ConsumerState<CartView> {
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    Future.microtask(() {
+      // ignore: unused_result
+      ref.refresh(getCartProvider);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,14 +38,16 @@ class CartView extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: data.isEmpty
+      body: widget.data.isEmpty
           ? NoItem(text: 'No Carts yet!')
           : CartViewBody(
-              data: data,
+              data: widget.data,
             ),
-      bottomNavigationBar: CartViewBottonNavigationBar(
-        data: data[0],
-      ),
+      bottomNavigationBar: widget.data.isNotEmpty
+          ? CartViewBottonNavigationBar(
+              data: widget.data[0],
+            )
+          : null,
     );
   }
 }
