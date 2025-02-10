6927550f62ad7b1ea819c91fbf4e8998e7c969b2
fady emil <fadymobiledev@gmail.com>
2025-02-10 23:39:58 +0200

    done repo imple

--- lib/features/orders/data/repo/orders_repo_impl.dart
+++ lib/features/orders/data/repo/orders_repo_impl.dart
@@ -6,8 +6,10 @@ import 'package:e_commerce/core/errors/faliures.dart';
 import 'package:e_commerce/core/services/api/api_service.dart';
 import 'package:e_commerce/features/orders/data/models/add_order_model.dart';
 import 'package:e_commerce/features/orders/data/models/get_order_model.dart';
+import 'package:e_commerce/features/orders/data/models/order_details.dart';
 import 'package:e_commerce/features/orders/domain/entites/add_orders_entity.dart';
 import 'package:e_commerce/features/orders/domain/entites/get_order_entity.dart';
+import 'package:e_commerce/features/orders/domain/entites/order_details_entity.dart';
 import 'package:e_commerce/features/orders/domain/repo/orders_repo.dart';
 
 class OrdersRepoImpl implements OrdersRepo {
@@ -48,6 +50,20 @@ class OrdersRepoImpl implements OrdersRepo {
       return Right(getOrderEntity);
     } catch (e) {
       log('Exception in getCategoriesDetails: $e');
+      throw Left(CustomException(message: 'حدث خطأ أثنا�� ��لب البيانات.'));
+    }
+  }
+
+  @override
+  Future<Either<Faliure, OrderDetailsEntity>> getOrderDetails({required String token, required int id})async {
+    try {
+      final data = await apiService.orderDetails(token, id);
+      log('API Response: $data');
+      final orderDetailsModel = OrderDetails.fromJson(data);
+      final orderDetailsEntity = orderDetailsModel.toEntity();
+      return Right(orderDetailsEntity);
+    } catch (e) {
+      log('Exception in getOrderDetails: $e');
       throw Left(CustomException(message: 'حدث خطأ أثنا�� ��لب البيانات.'));
     }
   }
